9148475a97be9b0ebdbe16273a681d6655969982
unknown <tony.netto.4528@gmail.com>
2020-05-06 14:57:06 -0300

    all

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+if (isset($_POST['task'])) {
+    $texto = $_POST['task'];
+  
+    $arquivo = fopen('task.txt', 'a');
+    fwrite($arquivo, $texto . "\n");
+    fclose($arquivo);
+}
+?>
+<?php
+$arquivo = 'task.txt';
+$fp = fopen($arquivo,'r');
+$texto = fread($fp, filesize($arquivo));
+$texto = nl2br($texto);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Lista PHP </title>
+</head>
+<body>
+    <div class="heading">   
+        <h2> Minha Lista </h2>
+    </div> 
+    <form method="POST" action="new.php">
+        <input type="text" name="task" class="task_input">
+        <button href="new.php" type="submit" class="add_bnt" name="submit">Adicionar</button>
+    </form> <br>
+    <table>
+        <thead>
+            <tr>
+                <th>Tarefas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class= "task"><p class="teste"> <? echo mb_strtolower( $texto, 'UTF-8' ); ?> </p></th>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
---
+++ new.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['task'])) {
+    $texto = $_POST['task'];
+  
+    $arquivo = fopen('task.txt', 'a');
+    fwrite($arquivo, $texto . "\n");
+    fclose($arquivo);
+}
+?>
+<?php
+$arquivo = 'task.txt';
+$fp = fopen($arquivo,'r');
+$texto = fread($fp, filesize($arquivo));
+$texto = nl2br($texto);
+?>
+<?php
+header('Location: https://tarefas4528.herokuapp.com/');
+?>
