f3fabd6c2b13f105bb6ba67137bcd53ddd518d82
unknown <tony.netto.4528@gmail.com>
2020-05-11 15:39:56 -0300

    all

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 $db = parse_url(getenv("postgres://hgeqjxmwfmbujl:1bde68c14fa3f627ae70c6107d5945d785b671c696e7839a7888aca6d17894b8@ec2-54-86-170-8.compute-1.amazonaws.com:5432/d11r24rr0egnvj"));
 $db["path"] = ltrim($db["path"], "/");
-if (isset($_POST['submit'])) {
-    $task = $_POST['task'];
-    mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
-    header('location: index.php');
-}
 ?>
 
 <!DOCTYPE html>
@@ -28,13 +23,9 @@ if (isset($_POST['submit'])) {
             </tr>
         </thead>
         <tbody>
-        <?php while ($row = mysqli_fetch_array($tasks)) { ?>
             <tr>
-                <td><?php echo $row ['id']; ?></td>
-                <td class= "task"><p class="teste"> <?php echo $row['task']; ?> </p></th>
+                <td class= "task"><p class="teste">  </p></th>
             </tr>
-        <?php } ?>
-           
         </tbody>
     </table>
 </body>
--- new.php
+++ new.php
@@ -1,9 +1,5 @@
 <?php
 $db = parse_url(getenv("postgres://hgeqjxmwfmbujl:1bde68c14fa3f627ae70c6107d5945d785b671c696e7839a7888aca6d17894b8@ec2-54-86-170-8.compute-1.amazonaws.com:5432/d11r24rr0egnvj"));
 $db["path"] = ltrim($db["path"], "/");
-if (isset($_POST['submit'])) {
-    $task = $_POST['task'];
-    mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
     header('location: /');
-}
 ?>
