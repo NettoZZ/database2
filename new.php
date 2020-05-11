8835b4ea8b7fbca35f142aed73afd0ccc39768fb
unknown <tony.netto.4528@gmail.com>
2020-05-11 16:36:27 -0300

    all

--- new.php
+++ new.php
@@ -1,5 +1,5 @@
 <?php
-$db = parse_url(getenv("postgres://hgeqjxmwfmbujl:1bde68c14fa3f627ae70c6107d5945d785b671c696e7839a7888aca6d17894b8@ec2-54-86-170-8.compute-1.amazonaws.com:5432/d11r24rr0egnvj"));
+$db = parse_url(getenv("POSTGRES_CONNECTION_STRING"));
 $db["path"] = ltrim($db["path"], "/");
     header('location: /');
 ?>
