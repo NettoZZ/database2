49792786be4c06dc086821a2cbbca1d9a00f6784
unknown <tony.netto.4528@gmail.com>
2020-05-11 15:43:07 -0300

    all

--- index.php
+++ index.php
@@ -24,7 +24,10 @@ $db["path"] = ltrim($db["path"], "/");
         </thead>
         <tbody>
             <tr>
-                <td class= "task"><p class="teste">  </p></th>
+                <td class= "task"><p class="teste"> <?php
+$db = parse_url(getenv("postgres://hgeqjxmwfmbujl:1bde68c14fa3f627ae70c6107d5945d785b671c696e7839a7888aca6d17894b8@ec2-54-86-170-8.compute-1.amazonaws.com:5432/d11r24rr0egnvj"));
+$db["path"] = ltrim($db["path"], "/");
+?> </p></th>
             </tr>
         </tbody>
     </table>
